d815417eec6517a0660d90665f3d24b8c680ba21
RustLi <332014208@qq.com>
2019-11-01 13:39:56 +0800

    add:增加ip地址工具类

---
+++ app/src/main/java/com/rustli/androidlearning/utils/IpAdressUtils.java
@@ -0,0 +1,304 @@
+package com.rustli.androidlearning.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * Created by czq on 2016/11/23.
+ * ip地址管理的工具类
+ */
+
+public class IpAdressUtils {
+    private static final String TAG = "IpAdressUtils";
+    /**
+     * gps获取ip
+     * @return
+     */
+    public static String getLocalIpAddress() {
+        try
+        {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();)
+            {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();)
+                {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress())
+                    {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        }
+        catch (Exception ex) {
+        }
+        return null;
+    }
+
+    /**
+     * wifi获取ip
+     * @param context
+     * @return
+     */
+    public static String getIp(Context context){
+        try {
+            //获取wifi服务
+            WifiManager wifiManager = (WifiManager)context. getSystemService(Context.WIFI_SERVICE);
+            //判断wifi是否开启
+            if (!wifiManager.isWifiEnabled()) {
+                wifiManager.setWifiEnabled(true);
+            }
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            int ipAddress = wifiInfo.getIpAddress();
+            String ip = intToIp(ipAddress);
+            return ip;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    /**
+     * 格式化ip地址（192.168.11.1）
+     * @param i
+     * @return
+     */
+    private static String intToIp(int i) {
+
+        return (i & 0xFF ) + "." +
+                ((i >> 8 ) & 0xFF) + "." +
+                ((i >> 16 ) & 0xFF) + "." +
+                ( i >> 24 & 0xFF) ;
+    }
+    /**
+     *  3G/4g网络IP
+     */
+    public static String getIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()
+                            && inetAddress instanceof Inet4Address) {
+                        // if (!inetAddress.isLoopbackAddress() && inetAddress
+                        // instanceof Inet6Address) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    /**
+     * 获取本机的ip地址（3中方法都包括）
+     * @param context
+     * @return
+     */
+    public static String getIpAdress(Context context){
+        Log.d(TAG, "getIpAdress: ");
+        String ip = null;
+        try {
+            ip=getIp(context);
+            if (ip==null){
+                ip = getIpAddress();
+                if (ip==null){
+                    ip = getLocalIpAddress();
+                }
+            }
+        } catch (Exception e) {
+        }
+        return ip;
+    }
+
+    public static String getIPAddress(Context context) {
+        NetworkInfo info = ((ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        if (info != null && info.isConnected()) {
+            if (info.getType() == ConnectivityManager.TYPE_MOBILE) {//当前使用2G/3G/4G网络
+                try {
+                    //Enumeration<NetworkInterface> en=NetworkInterface.getNetworkInterfaces();
+                    for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                        NetworkInterface intf = en.nextElement();
+                        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                            InetAddress inetAddress = enumIpAddr.nextElement();
+                            if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                                return inetAddress.getHostAddress();
+                            }
+                        }
+                    }
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }
+
+            } else if (info.getType() == ConnectivityManager.TYPE_WIFI) {//当前使用无线网络
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                String ipAddress = intIP2StringIP(wifiInfo.getIpAddress());//得到IPV4地址
+                return ipAddress;
+            }
+        } else {
+            //当前无网络连接,请在设置中打开网络
+        }
+        return null;
+    }
+
+    /**
+     * 将得到的int类型的IP转换为String类型
+     *
+     * @param ip
+     * @return
+     */
+    public static String intIP2StringIP(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                (ip >> 24 & 0xFF);
+    }
+
+
+    /**
+     * 得到无线网关的IP地址
+     *
+     * @return
+     */
+    private void getAllIp() {
+
+        try {
+            // 获取本地设备的所有网络接口
+            Enumeration<NetworkInterface> enumerationNi = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumerationNi.hasMoreElements()) {
+                NetworkInterface networkInterface = enumerationNi.nextElement();
+                String interfaceName = networkInterface.getDisplayName();
+                Log.i("tag", "网络名字" + interfaceName);
+
+                Enumeration<InetAddress> enumIpAddr = networkInterface
+                        .getInetAddresses();
+
+                while (enumIpAddr.hasMoreElements()) {
+                    // 返回枚举集合中的下一个IP地址信息
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    Log.i("tag", inetAddress.getHostAddress() + "哪个类型的   "+inetAddress.getClass().toString());
+
+                }
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 得到无线网关的IP地址
+     *
+     * @return
+     */
+    public static String getWirelseeIp() {
+        Log.d(TAG, "getWirelseeIp: ");
+        try {
+            // 获取本地设备的所有网络接口
+            Enumeration<NetworkInterface> enumerationNi = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumerationNi.hasMoreElements()) {
+                NetworkInterface networkInterface = enumerationNi.nextElement();
+                String interfaceName = networkInterface.getDisplayName();
+                Log.i(TAG, "网络名字" + interfaceName);
+
+                // 如果是无线网卡
+                if (interfaceName.equals("wlan0")) {
+                    Enumeration<InetAddress> enumIpAddr = networkInterface
+                            .getInetAddresses();
+                    while (enumIpAddr.hasMoreElements()) {
+                        // 返回枚举集合中的下一个IP地址信息
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        // 不是回环地址，并且是ipv4的地址
+                        if (!inetAddress.isLoopbackAddress()
+                                && inetAddress instanceof Inet4Address) {
+                            Log.i("tag", inetAddress.getHostAddress() + "   ");
+                            return inetAddress.getHostAddress();
+                        }
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    /**
+     * 得到有限网关的IP地址
+     *
+     * @return
+     */
+    public static String getLocalIp() {
+
+        try {
+            // 获取本地设备的所有网络接口
+            Enumeration<NetworkInterface> enumerationNi = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumerationNi.hasMoreElements()) {
+                NetworkInterface networkInterface = enumerationNi.nextElement();
+                String interfaceName = networkInterface.getDisplayName();
+                Log.i("tag", "网络名字" + interfaceName);
+
+                // 如果是有限网卡
+                if (interfaceName.equals("eth0")) {
+                    Enumeration<InetAddress> enumIpAddr = networkInterface
+                            .getInetAddresses();
+
+                    while (enumIpAddr.hasMoreElements()) {
+                        // 返回枚举集合中的下一个IP地址信息
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        // 不是回环地址，并且是ipv4的地址
+                        if (!inetAddress.isLoopbackAddress()
+                                && inetAddress instanceof Inet4Address) {
+                            Log.i("tag", inetAddress.getHostAddress() + "   ");
+
+                            return inetAddress.getHostAddress();
+                        }
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    public static String getAllIpAdress(Context context){
+        String ip = "";
+        try {
+            ip=getLocalIp();
+            if (ip==null){
+                ip = getWirelseeIp();
+                if (ip==null){
+                    ip = getIpAdress(context);
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "getAllIpAdress: e = " + e.getMessage());
+        }
+        return ip;
+    }
+}
+
